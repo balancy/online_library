d31094798cde56ecc2e6cb3a33d05d6616a779d3
balancy <balancy@gmail.com>
2021-04-04 14:49:10 +0200

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,7 +1,9 @@
 import json
+import os
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
+from more_itertools import sliced
 
 
 def read_library_info():
@@ -13,38 +15,43 @@ def read_library_info():
     with open("library.json", "r", encoding="utf-8") as file:
         library = json.load(file)
 
-    return library
+    return list(sliced(library, 20))
 
 
-def rebuild(env):
+def rebuild(template):
     """Rebuild index.html every time template.html is changed.
 
-    :param env: environment
+    :param template: template to follow
     """
 
-    template = env.get_template("template.html")
+    library_chunked = read_library_info()
 
-    rendered_page = template.render(
-        library=read_library_info(),
-    )
+    for chunk_number, library_piece in enumerate(library_chunked):
+        rendered_page = template.render(
+            library=library_piece,
+        )
 
-    with open("index.html", "w", encoding="utf-8") as file:
-        file.write(rendered_page)
+        with open(f"pages/index{chunk_number}.html", "w", encoding="utf-8") as file:
+            file.write(rendered_page)
 
 
 if __name__ == "__main__":
     env = Environment(
-        loader=FileSystemLoader('.'),
+        loader=FileSystemLoader("."),
         autoescape=select_autoescape(
-            enabled_extensions=('html',),
+            enabled_extensions=("html",),
             default_for_string=True,
             default=True,
             )
     )
 
-    rebuild(env)
+    os.makedirs("pages/books/", exist_ok=True)
+    os.makedirs("pages/images/", exist_ok=True)
+
+    template = env.get_template("template.html")
+    rebuild(template)
 
     server = Server()
-    server.watch('template.html', rebuild)
+    server.watch("template.html", rebuild)
 
-    server.serve(root='.')
+    server.serve(root='pages', default_filename="index0.html")
